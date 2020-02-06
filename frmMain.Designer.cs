01cfed5ee9f17144924b8f5bf66a3907d19af636
Mark <markh@rris.com.au>
2020-02-06 05:58:42 +0000

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace HaasEmulator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.txtTool = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTool)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.txtPort);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 52);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server Control";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(38, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Text = "5100";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(144, 17);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtMessages);
+            this.groupBox2.Location = new System.Drawing.Point(12, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(589, 368);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Messages";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(607, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tool Number";
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.Location = new System.Drawing.Point(9, 19);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.Size = new System.Drawing.Size(574, 343);
+            this.txtMessages.TabIndex = 0;
+            // 
+            // txtTool
+            // 
+            this.txtTool.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTool.Location = new System.Drawing.Point(607, 86);
+            this.txtTool.Name = "txtTool";
+            this.txtTool.Size = new System.Drawing.Size(181, 20);
+            this.txtTool.TabIndex = 4;
+            this.txtTool.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.txtTool.ValueChanged += new System.EventHandler(this.txtTool_ValueChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTool);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmMain";
+            this.Text = "Haas Toolchanger Emulator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTool)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtMessages;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown txtTool;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,167 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HaasEmulator
+{
+    public partial class frmMain : Form
+    {
+        SimpleTcpServer server = null;
+        int toolChanges = 1;
+        DateTime start = DateTime.Now;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (server == null)
+            {
+                int port = 0;
+                Int32.TryParse(txtPort.Text, out port);
+                if (port == 0)
+                {
+                    MessageBox.Show("Enter a port...", "No port", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                start = DateTime.Now;
+
+                server = new SimpleTcpServer().Start(port);
+                server.Delimiter = (byte)'\n';
+                server.DelimiterDataReceived += Server_DelimiterDataReceived;
+                server.ClientConnected += (s, ev) => { AppendMessage("New Client connected."); };
+                server.ClientDisconnected += (s, ev) => { AppendMessage("Client disconnected."); };
+
+                AppendMessage($"Server started on port {port}");
+
+                btnStart.Text = "Stop";
+            }
+            else
+            {
+                server.Stop();
+                server = null;
+
+                AppendMessage("Server stopped.");
+
+                btnStart.Text = "Start";
+            }
+
+        }
+
+        private void Server_DelimiterDataReceived(object sender, SimpleTCP.Message e)
+        {
+            if (string.IsNullOrWhiteSpace(e.MessageString))
+            {
+                AppendMessage("Empty message received.");
+                return;
+            }
+
+            string message = e.MessageString.Trim();
+
+            if (!message.Contains('?'))
+            {
+                AppendMessage($"Message does not appear to have a query in it: '{message}'.");
+                return;
+            }
+
+            message = message.Substring(message.IndexOf('?'));
+
+            string reply = null;
+            var dif = DateTime.Now - start;
+
+            switch (message)
+            {
+                case "?Q100":
+                    AppendMessage("?Q100 - Machine Serial Number.");
+                    reply = $"SERIAL NUMBER, 1234567";
+                    break;
+                case "?Q101":
+                    AppendMessage("?Q101 - Control Software Version.");
+                    reply = $"SOFTWARE VERSION, 100.17.000.2037";
+                    break;
+                case "?Q102":
+                    AppendMessage("?Q102 - Machine Model Number.");
+                    reply = $"MODEL, CSMD-G2";
+                    break;
+                case "?Q104":
+                    AppendMessage("?Q104 - Mode (LIST PROG, MDI, etc.).");
+                    reply = $"MODE, ZERO";
+                    break;
+                case "?Q200":
+                    AppendMessage("?Q200 - Tool Changes (total).");
+                    reply = $"TOOL CHANGES, {toolChanges}";
+                    break;
+                case "?Q201":
+                    AppendMessage("?Q201 - Tool Number in use.");
+                    reply = $"USING TOOL, {txtTool.Value}";
+                    break;
+                case "?Q300":
+                    AppendMessage("?Q300 - Power-on Time (total).");
+                    reply = $"P.O. TIME, {dif.TotalHours:00000}:{dif.Minutes}:{dif.Seconds}";
+                    break;
+                case "?Q301":
+                    AppendMessage("?Q301 - Motion Time (total).");
+                    reply = $"C.S. TIME, {dif.TotalHours:00000}:{dif.Minutes}:{dif.Seconds}";
+                    break;
+                case "?Q303":
+                    AppendMessage("?Q303 - Last Cycle Time.");
+                    reply = $"LAST CYCLE, 00000:00:13";
+                    break;
+                case "?Q304":
+                    AppendMessage("?Q304 - Previous Cycle Time.");
+                    reply = $"PREV CYCLE, 00000:00:01";
+                    break;
+                case "?Q402":
+                    AppendMessage("?Q402 - M30 Parts Counter #1 (resettable at control).");
+                    reply = $"M30 #1, 380";
+                    break;
+                case "?Q403":
+                    AppendMessage("?Q403 - M30 Parts Counter #2 (resettable at control).");
+                    reply = $"M30 #2, 380";
+                    break;
+                case "?Q500":
+                    AppendMessage("?Q500 - Three-in-one (PROGRAM, Oxxxxx, STATUS, PARTS, xxxxx.");
+                    reply = $"PROGRAM, MDI, IDLE, PARTS, 380";
+                    break;
+                default:
+                    AppendMessage($"Unknown message: '{message}'");
+                    break;
+            }
+
+            e.ReplyLine(reply);
+            AppendMessage(reply);
+        }
+
+        private void AppendMessage(string message)
+        {
+            message = $"{Environment.NewLine}[{DateTime.Now:HH:mm:ss}] {message}";
+
+            if (txtMessages.InvokeRequired)
+            {
+                txtMessages.Invoke(new Action(() =>
+                {
+                    txtMessages.AppendText(message);
+                }));
+            }
+            else
+            {
+                txtMessages.AppendText(message);
+            }
+        }
+
+        private void txtTool_ValueChanged(object sender, EventArgs e)
+        {
+            toolChanges += 1;
+        }
+    }
+}
